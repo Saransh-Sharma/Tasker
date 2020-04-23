74ca717586a7889f1ce2f8e5f5892257b5e9d400
Saransh Sharma <saransh1337@gmail.com>
2020-04-23 21:05:39 +0530

    weekly & inbox view controllers

---
+++ To Do List/InboxViewController.swift
@@ -0,0 +1,49 @@
+//
+//  SecondViewController.swift
+//  To Do List
+//
+//  Created by Saransh Sharma on 22/04/20.
+//  Copyright © 2020 saransh1337. All rights reserved.
+//
+
+import UIKit
+
+class InboxViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Inbox"
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let inboxCell = UITableViewCell()
+        
+        inboxCell.textLabel?.text = "This is inbox cell \(indexPath.row)"
+        
+        return inboxCell
+    }
+    
+    
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
