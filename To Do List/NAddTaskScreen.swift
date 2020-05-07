578e7ad2d2e9bf038911ed7b768abe3d47c150bf
Saransh Sharma <saransh1337@gmail.com>
2020-05-07 19:31:58 +0530

    add clear button on the redesigned add task screen

--- To Do List/NAddTaskScreen.swift
+++ To Do List/NAddTaskScreen.swift
@@ -14,101 +14,123 @@ class NAddTaskScreen: UIViewController, UITableViewDataSource, UITableViewDelega
     var secondryColor =  #colorLiteral(red: 0.2039215686, green: 0, blue: 0.4078431373, alpha: 1)
     
     
-    let addTaskTextField = UITextView()
+    
     let seperatorCellID = "seperator"
     // MARK:- Outlets
     
     @IBOutlet weak var weeklyTableView: UITableView!
     
+    @IBOutlet weak var addTaskTextField: UITextField!
+    
+    
+    
+    //    override func loadView() {
+    //        super.loadView()
+    ////        view.addSubview(servePageHeader())
+    ////        setupTableView()
+    //    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(setupAddTaskPageHeader())
+        weeklyTableView.delegate = self
+        weeklyTableView.dataSource = self
+        
+        setupAddTaskTextField(textFeild: addTaskTextField)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        if (indexPath.row == 0) {
+            return view.bounds.height/10
+        }
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cell = UITableViewCell()
+        cell.textLabel?.numberOfLines = 0
+        
+        
+        
+        if (indexPath.row == 0) {
+            cell = tableView.dequeueReusableCell(withIdentifier: seperatorCellID, for: indexPath)
+            cell.backgroundColor = secondryColor
+            
+            //               // cell.textLabel?.text = "This is compact \(indexPath.row)"
+            //                cell.backgroundColor = secondryColor
+            ////                addTaskTextField.placeholde
+            //
+            //                let textField = UITextField()
+            ////                textField.frame = CGRect(x: cell.bounds.minX, y: cell.bounds.minY, width: cell.bounds.width, height: cell.bounds.width)
+            
+            //                cell.addSubview(textField)
+            
+            
+            
+        } else {
+            cell.textLabel?.text = "This is a really long title that has no hope of fittinng in. blah blah blah.... blah blah blah.... blah blah blah.... \(indexPath.row)"
+        }
+        
+        
+        //        weeklyTaskTitleLabel.text = "This is weekly Cell \(indexPath.row)"
+        //inboxCell.textLabel?.text = "This is weekly cell \(indexPath.row)"
+        
+        return cell
+    }
+    
+    //    func setupTableView() {
+    //        view.addSubview(tableView)
+    //        tableView.translatesAutoresizingMaskIntoConstraints = false
+    //        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+    //        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+    //        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    //        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+    //
+    //        tableView.register(UITableViewCell.self, forCellReuseIdentifier: seperatorCellID)
+    //    }
+    
+    
+    func setupAddTaskTextField(textFeild: UITextField) {
+        
+        let placeholderString =
+            NSAttributedString.init(string: "Type in & tap done", attributes: [NSAttributedString.Key.foregroundColor : primaryColor])
+        
+        textFeild.attributedPlaceholder = placeholderString
+        textFeild.backgroundColor = UIColor.blue
+    
+        
  
-    
-    
-    
-//    override func loadView() {
-//        super.loadView()
-////        view.addSubview(servePageHeader())
-////        setupTableView()
-//    }
-    
-     override func viewDidLoad() {
-            super.viewDidLoad()
+//        UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+//        [button setImage:[UIImage imageNamed:@"clear_button.png"] forState:UIControlStateNormal];
+//        [button setFrame:CGRectMake(0.0f, 0.0f, 15.0f, 15.0f)]; // Required for iOS7
+//        theTextField.rightView = button;
+//        theTextField.rightViewMode = UITextFieldViewModeWhileEditing;
         
-            view.addSubview(setupAddTaskPageHeader())
-            weeklyTableView.delegate = self
-            weeklyTableView.dataSource = self
-            
-        }
-        
-        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        
-            if (indexPath.row == 0) {
-                return view.bounds.height/10
-            }
-            return UITableView.automaticDimension
-        }
-
-        func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
-            return UITableView.automaticDimension
-        }
-        
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return 5
-        }
-        
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            
-            var cell = UITableViewCell()
-            cell.textLabel?.numberOfLines = 0
-            
-            
-            
-            if (indexPath.row == 0) {
-                cell = tableView.dequeueReusableCell(withIdentifier: seperatorCellID, for: indexPath)
-                cell.backgroundColor = secondryColor
-                
-//               // cell.textLabel?.text = "This is compact \(indexPath.row)"
-//                cell.backgroundColor = secondryColor
-////                addTaskTextField.placeholde
-//
-//                let textField = UITextField()
-////                textField.frame = CGRect(x: cell.bounds.minX, y: cell.bounds.minY, width: cell.bounds.width, height: cell.bounds.width)
-//                let placeholderString =
-//                    NSAttributedString.init(string: "UITextField Demo", attributes: [NSAttributedString.Key.foregroundColor : primaryColor])
-//
-//                textField.attributedPlaceholder = placeholderString
-//                cell.addSubview(textField)
-                
-                
-                
-            } else {
-                cell.textLabel?.text = "This is a really long title that has no hope of fittinng in. blah blah blah.... blah blah blah.... blah blah blah.... \(indexPath.row)"
-            }
-            
-            
-    //        weeklyTaskTitleLabel.text = "This is weekly Cell \(indexPath.row)"
-            //inboxCell.textLabel?.text = "This is weekly cell \(indexPath.row)"
-            
-            return cell
-        }
-    
-//    func setupTableView() {
-//        view.addSubview(tableView)
-//        tableView.translatesAutoresizingMaskIntoConstraints = false
-//        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-//        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-//        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-//        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-//        
-//        tableView.register(UITableViewCell.self, forCellReuseIdentifier: seperatorCellID)
-//    }
+        let mButton = UIButton(type: .custom)
+        mButton.setImage(UIImage(named: "icon_close"), for: UIControl.State.normal)
+        textFeild.rightView = mButton
+        textFeild.rightViewMode = .whileEditing
+    }
     
     
     
     func setupAddTaskPageHeader() -> UIView {
         let view = UIView(frame: UIScreen.main.bounds)
-//        view.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 128)
+        //        view.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 128)
         view.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height/7+10)
-//        view.backgroundColor = secondryColor
+        //        view.backgroundColor = secondryColor
         view.backgroundColor = .green
         
         //            let homeTitle = UILabel()
@@ -127,8 +149,8 @@ class NAddTaskScreen: UIViewController, UITableViewDataSource, UITableViewDelega
 }
 
 
-    
-    
-    
+
+
+
 
 
