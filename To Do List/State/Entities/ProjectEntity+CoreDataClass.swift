492f82b76cdd3fe6798435a7edba511919cde60e
Saransh <saransh1337@gmail.com>
2026-02-20 03:23:10 +0530

    feat: add V3 CoreData entities and data model

---
+++ To Do List/State/Entities/ProjectEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ProjectEntity+CoreDataClass.swift
+//  
+//
+//  Created by Saransh Sharma on 29/06/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ProjectEntity)
+public class ProjectEntity: NSManagedObject {
+
+}
