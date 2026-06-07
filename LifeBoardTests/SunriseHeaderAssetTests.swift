270cc92169199314ef80f648ca6ec1a15ae4d22b
Saransh <saransh1337@gmail.com>
2026-06-07 16:50:41 +0530

    refactor: deduplicate contrast helper methods across test files
    Remove inline resolvedColor, contrastRatio, and relativeLuminance
    helpers from SunriseHeaderAssetTests and AppOnboardingTests, and
    use the shared versions from SharedColorTestHelpers instead.

--- LifeBoardTests/SunriseHeaderAssetTests.swift
+++ LifeBoardTests/SunriseHeaderAssetTests.swift
@@ -821,60 +821,6 @@ final class SunriseHeaderAssetTests: XCTestCase {
         )
     }
 
-    #if canImport(UIKit)
-    private func resolvedColor(
-        _ color: Color,
-        style: UIUserInterfaceStyle,
-        contrast: UIAccessibilityContrast = .normal
-    ) -> UIColor {
-        let traits = UITraitCollection(traitsFrom: [
-            UITraitCollection(userInterfaceStyle: style),
-            UITraitCollection(accessibilityContrast: contrast)
-        ])
-        return UIColor(color).resolvedColor(with: traits)
-    }
-
-    private func contrastRatio(_ foreground: UIColor, _ background: UIColor) -> CGFloat {
-        let lighter = max(relativeLuminance(foreground), relativeLuminance(background))
-        let darker = min(relativeLuminance(foreground), relativeLuminance(background))
-        return (lighter + 0.05) / (darker + 0.05)
-    }
-
-    private func relativeLuminance(_ color: UIColor) -> CGFloat {
-        let components = rgbaComponents(color)
-        func channel(_ value: CGFloat) -> CGFloat {
-            value <= 0.03928 ? value / 12.92 : pow((value + 0.055) / 1.055, 2.4)
-        }
-        return 0.2126 * channel(components.red)
-            + 0.7152 * channel(components.green)
-            + 0.0722 * channel(components.blue)
-    }
-
-    private func redComponent(_ color: UIColor) -> CGFloat {
-        rgbaComponents(color).red
-    }
-
-    private func greenComponent(_ color: UIColor) -> CGFloat {
-        rgbaComponents(color).green
-    }
-
-    private func blueComponent(_ color: UIColor) -> CGFloat {
-        rgbaComponents(color).blue
-    }
-
-    private func alphaComponent(_ color: UIColor) -> CGFloat {
-        rgbaComponents(color).alpha
-    }
-
-    private func rgbaComponents(_ color: UIColor) -> (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
-        var red: CGFloat = 0
-        var green: CGFloat = 0
-        var blue: CGFloat = 0
-        var alpha: CGFloat = 0
-        color.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
-        return (red, green, blue, alpha)
-    }
-    #endif
 }
 
 #if canImport(UIKit)
@@ -1069,54 +1015,4 @@ final class TaskDetailStyleTests: XCTestCase {
     }
 }
 
-private func resolvedColor(
-    _ color: Color,
-    style: UIUserInterfaceStyle,
-    contrast: UIAccessibilityContrast = .normal
-) -> UIColor {
-    let traits = UITraitCollection(traitsFrom: [
-        UITraitCollection(userInterfaceStyle: style),
-        UITraitCollection(accessibilityContrast: contrast)
-    ])
-    return UIColor(color).resolvedColor(with: traits)
-}
-
-private func contrastRatio(_ foreground: UIColor, _ background: UIColor) -> CGFloat {
-    let lighter = max(relativeLuminance(foreground), relativeLuminance(background))
-    let darker = min(relativeLuminance(foreground), relativeLuminance(background))
-    return (lighter + 0.05) / (darker + 0.05)
-}
-
-private func composited(_ foreground: UIColor, over background: UIColor) -> UIColor {
-    let foregroundComponents = rgbaComponents(foreground)
-    let backgroundComponents = rgbaComponents(background)
-    let foregroundAlpha = foregroundComponents.alpha
-    let backgroundAlpha = backgroundComponents.alpha * (1 - foregroundAlpha)
-    let alpha = foregroundAlpha + backgroundAlpha
-    guard alpha > 0 else { return .clear }
-
-    let red = (foregroundComponents.red * foregroundAlpha + backgroundComponents.red * backgroundAlpha) / alpha
-    let green = (foregroundComponents.green * foregroundAlpha + backgroundComponents.green * backgroundAlpha) / alpha
-    let blue = (foregroundComponents.blue * foregroundAlpha + backgroundComponents.blue * backgroundAlpha) / alpha
-    return UIColor(red: red, green: green, blue: blue, alpha: alpha)
-}
-
-private func relativeLuminance(_ color: UIColor) -> CGFloat {
-    let components = rgbaComponents(color)
-    func channel(_ value: CGFloat) -> CGFloat {
-        value <= 0.03928 ? value / 12.92 : pow((value + 0.055) / 1.055, 2.4)
-    }
-    return 0.2126 * channel(components.red)
-        + 0.7152 * channel(components.green)
-        + 0.0722 * channel(components.blue)
-}
-
-private func rgbaComponents(_ color: UIColor) -> (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
-    var red: CGFloat = 0
-    var green: CGFloat = 0
-    var blue: CGFloat = 0
-    var alpha: CGFloat = 0
-    color.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
-    return (red, green, blue, alpha)
-}
 #endif
